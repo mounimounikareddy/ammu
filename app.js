4c582d100378eb2385eaf0b5d8e4d8f8764e7cc2
mounimounikareddy <165641584+mounimounikareddy@users.noreply.github.com>
2024-04-25 14:34:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 let main=document.getElementById("java")
 
-function votevalidation(){
-    let paragraph =document.getElementById("para")
-    let age=document.getElementById("age-num").value
-    // console.log(age)
-    if(age>=18){
-        paragraph.innerHTML="eligble for vote"
-    }
-    else if(age==""){
-paragraph.innerHTML="please enter your age"
-    }
-    else{
-        paragraph.innerHTML="not eligble for vote"
-    }
+function taxcalculate(){
+    let actualSalary =document.getElementById("para")
+    let tax =document.getElementById("tax")
+    let hand =document.getElementById("hand")
+    let salary=document.getElementById("your-sal").value
+     let a=salary*0.1
+     let b=salary-a
+    
+     actualSalary.innerHTML="total salary:"+salary
+     tax.innerHTML="tax:"+a
+     hand.innerHTML="hand salary:"+b
+
 }
